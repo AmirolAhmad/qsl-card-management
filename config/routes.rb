751725de0e975f667e48374c5dd76e1a0f67c0b3
Amirol Ahmad <9w2pdf@gmail.com>
2017-04-09 21:50:29 +0800

    setup user with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  devise_scope :user do
+    get '/login' => 'devise/sessions#new'
+    get '/register' => 'devise/registrations#new'
+    get '/settings' => 'devise/registrations#edit'
+  end
 end
