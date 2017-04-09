ab0fe91fd6efaa2a32c9f4ee460aa66f3da440c1
Amirol Ahmad <9w2pdf@gmail.com>
2017-04-10 02:18:11 +0800

    setup tracking card

---
+++ app/controllers/tracks_controller.rb
@@ -0,0 +1,74 @@
+class TracksController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_callsign, only: [:index, :show, :edit, :create, :update, :destroy]
+  before_action :set_track, only: [:index, :show, :edit, :create, :update, :destroy]
+  before_action :find_track, only: [:show, :edit, :update, :destroy]
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @track = Track.new
+  end
+
+  def edit
+  end
+
+  def create
+    @track = @tracks.new(track_params)
+    respond_to do |format|
+      if @track.save
+        format.html { redirect_to callsign_track_path(id:@track), notice: 'Track was successfully created.' }
+        format.json { render :show, status: :created, location: @track }
+      else
+        format.html { render :new }
+        format.json { render json: @track.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @track.update(track_params)
+        format.html { redirect_to callsign_track_path(id:@track), notice: 'Track was successfully updated.' }
+        format.json { render :show, status: :ok, location: @track }
+      else
+        format.html { render :edit }
+        format.json { render json: @track.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @track.destroy
+    respond_to do |format|
+      format.html { redirect_to callsign_tracks_url, notice: 'Track was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+    def set_callsign
+      @callsign = current_user.callsigns.friendly.find params[:callsign_id]
+      rescue ActiveRecord::RecordNotFound
+        redirect_to callsigns_url, :notice => 'Record not found'
+    end
+
+    def set_track
+      @tracks = Track.where(callsign_id: @callsign.id)
+    end
+
+    def find_track
+      @track = @tracks.find params[:id]
+      rescue ActiveRecord::RecordNotFound
+        redirect_to callsign_tracks_url, :notice => 'Record not found'
+    end
+
+    def track_params
+      params.require(:track).permit(:callsign_id, :method, :date_in, :date_out, :status)
+    end
+end
--- app/models/track.rb
+++ app/models/track.rb
@@ -1,5 +1,5 @@
 class Track < ApplicationRecord
 enum method: [:tks, :pse]
 enum status: [:pending, :complete]
-  belongs_to :callsign
+belongs_to :callsign
 end
