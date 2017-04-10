f952ad7d4a56da171285a191e3e405b57148b048
Amirol Ahmad <9w2pdf@gmail.com>
2017-04-10 19:19:58 +0800

    add dependent destroy

--- app/models/callsign.rb
+++ app/models/callsign.rb
@@ -1,7 +1,7 @@
 class Callsign < ApplicationRecord
   extend FriendlyId
   belongs_to :user
-  has_many :tracks
+  has_many :tracks, dependent: :destroy
 
   friendly_id :callsign, use: [:slugged, :finders, :history]
 
